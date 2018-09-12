842ca56adb69084f088f1e9860235b8de0ad6ae0
Finbar Ryan <finbarr@microsoft.com>
2018-09-12 18:07:01 +0100

    Adding Content Type

--- BlobTriggerJS/index.js
+++ BlobTriggerJS/index.js
@@ -1,4 +1,14 @@
 module.exports = function (context, myBlob) {
+    const contentType = context.bindingData.properties.contentType;
     context.log("JavaScript blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", myBlob.length, "Bytes");
+    context.log('JavaScript blob trigger function is looking at: ', contentType);
+    if (contentType.includes("image")) {
+        // Read image with Jimp
+        context.log('This worked');
+      } else {
+        context.log(`Cannot process the file with content type:`, contentType);
+        context.done();
+      }
+
     context.done();
 };
